43f5d7b3441a33dc222dcef41812955f68713891
Zbien <102765463+Zbienn@users.noreply.github.com>
2025-05-22 16:56:14 +0100

    How to play Option

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject howToPlayPanel;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("GameScene");
@@ -12,5 +14,15 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Sair do jogo (apenas funciona na build)");
+    }
+
+    public void ShowHowToPlay()
+    {
+        howToPlayPanel.SetActive(true);
+    }
+
+    public void HideHowToPlay()
+    {
+        howToPlayPanel.SetActive(false);
     }
 }
